4d803b680f65d645e9a8848faf37e426680b138b
mikforce@github.com <mikforce4378403@gmail.com>
2022-04-25 21:54:38 +0300

    second commit

---
+++ app/controllers/tamplates_controller.rb
@@ -0,0 +1,37 @@
+class TamplatesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    Template.create(
+      name: params[:tamplate][:name],
+      chernovik: params[:tamplate][:chernovik],
+      opisanie: params[:tamplate][:opisanie],
+      types: params[:tamplate][:types],
+      koef: params[:tamplate][:koef]
+    )
+  end
+
+  def update
+    @tamplate = Tamplate.find(params[:id])
+    @tamplate.update(
+      name: params[:tamplate][:name],
+      chernovik: params[:tamplate][:chernovik],
+      opisanie: params[:tamplate][:opisanie],
+      types: params[:tamplate][:types],
+      koef: params[:tamplate][:koef]
+    )
+  end
+
+  def delete
+    @tamplate = Tamplate.find(params[:id])
+    @tamplate.destroy
+  end
+
+  def show
+    @tamplate = Tamplate.find(params[:id])
+  end
+
+  def index
+    @tamplates = Tamplate.all
+  end
+end
